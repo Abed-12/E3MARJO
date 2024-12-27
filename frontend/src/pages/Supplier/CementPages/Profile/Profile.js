c37e5d53c5e5a148f2dd8251f8e401c34dee9bcc
Saif Al-Naimat <saif.alnaimat@progressoft.com>
2024-12-28 00:38:47 +0300

    fix: fix download file button on supplier profile page.

--- frontend/src/pages/Supplier/CementPages/Profile/Profile.js
+++ frontend/src/pages/Supplier/CementPages/Profile/Profile.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import {jwtDecode} from "jwt-decode";
-import { handleError, handleSuccess } from '../../../../utils/utils';
-import { ToastContainer } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import {handleError, handleSuccess} from '../../../../utils/utils';
+import {ToastContainer} from 'react-toastify';
+import {useNavigate} from 'react-router-dom';
 import styles from './Profile.module.css';
 import Navbar from '../../../../components/navbar/Navbar';
 import Footer from '../../../../components/footer/Footer';
+import {saveAs} from 'file-saver';
 
 function Profile() {
     const token = localStorage.getItem("token");
@@ -22,10 +23,10 @@ function Profile() {
         setTimeout(() => {
             navigate('/supplier-login');
         }, 500)
-    }    
+    }
 
     const handleEditPriceClick = () => {
-        navigate("/supplier/cement/profile/edit-profile"); 
+        navigate("/supplier/cement/profile/edit-profile");
     };
 
     const fetchSupplierData = async () => {
@@ -49,9 +50,26 @@ function Profile() {
         fetchSupplierData();
     }, []);
 
-    return(
+    const downloadCommercialRegisterPdf = async () => {
+        try {
+            const url = `http://localhost:8080/auth/supplier/supplier-commercial-register`;
+            const options = {
+                method: 'GET',
+                headers: {Authorization: localStorage.getItem('token')}
+            }
+            const response = await fetch(url, options);
+            const contentDisposition = response.headers.get('content-disposition');
+            const filename = contentDisposition ? contentDisposition.split('filename=')[1].replace(/"/g, '') : 'file.pdf';
+            const file = await response.blob();
+            saveAs(file, filename)
+        } catch (err) {
+            handleError(err);
+        }
+
+    }
+    return (
         <section className={styles.profileBody}>
-            <Navbar 
+            <Navbar
                 two="Orders"
                 two1="Under preparing orders"
                 pathTwo1="/supplier/cement/under-preparing-orders"
@@ -67,21 +85,22 @@ function Profile() {
             <div className={styles.profileContainer}>
                 <div className={styles.profileRow}>
                     <h1 className={styles.profileH1}>{decodedData.supplierName} Profile</h1>
-                    <button className={styles.profileEditPriceButton} onClick={handleEditPriceClick}>Edit Profile</button>
+                    <button className={styles.profileEditPriceButton} onClick={handleEditPriceClick}>Edit Profile
+                    </button>
                     <p><strong>Supplier name:</strong> {decodedData.supplierName}</p>
                     <p><strong>Supplier ID:</strong> {decodedData.supplierID}</p>
                     <p><strong>Email:</strong> {decodedData.email}</p>
                     <p><strong>Phone:</strong> {supplierData.supplierPhone}</p>
                     <p><strong>Product:</strong> {decodedData.supplierProduct}</p>
                     <p>
-                        <strong>Commercial register: </strong> 
-                        <a href={`http://localhost:5000/uploads/${supplierData.commercialRegister}`} target="_blank" rel="noopener noreferrer">View PDF</a>
+                        <strong>Commercial register: </strong>
+                        <button onClick={downloadCommercialRegisterPdf}>Download PDF</button>
                     </p>
                     <p><strong>Price of one bag:</strong> {supplierData.price} JD</p>
                 </div>
             </div>
 
-            <Footer 
+            <Footer
                 two="Orders"
                 two1="Under preparing orders"
                 pathTwo1="/supplier/cement/under-preparing-orders"
@@ -93,9 +112,9 @@ function Profile() {
                 pathFour="/supplier/cement/profile"
                 logout={handleLogout}
             />
-            <ToastContainer />
+            <ToastContainer/>
         </section>
     );
 }
 
-export { Profile };
+export {Profile};
