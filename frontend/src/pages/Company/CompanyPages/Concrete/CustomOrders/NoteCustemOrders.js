f1c710e806a0e0daafa3071ad55fe361afccdefb
direvain <habboush.abdulrahman@gmail.com>
2025-01-03 22:44:06 +0300

    s

--- frontend/src/pages/Company/CompanyPages/Concrete/CustomOrders/NoteCustemOrders.js
+++ frontend/src/pages/Company/CompanyPages/Concrete/CustomOrders/NoteCustemOrders.js
@@ -39,11 +39,74 @@ function NoteCustomOrders() {
                 logout={handleLogout}
             />
     <div>
+        <div className={styles.noteContainer}>
+            <div className={styles.steps}>
+                <ol>
+                    <li>
+                        Select a Supplier
+                            <ul>
+                                <li>
+                                    Choose your preferred supplier from the dropdown menu
+                                </li>
+                            </ul>
+                    </li>
+                    <li>
+                        Specify the Number of Concrete Pouring Locations(Number of rows):
+                        <ul>
+                            <li>
+                                Enter the total number of concrete pouring locations you need to calculate concrete for. 
+                                <br></br>
+                                The form will generate the exact number of rows based on your input.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Fill in the Details for Each Concrete Pouring Location(item name):
+                        <ul>
+                            <li>
+                                Item  Name: Enter the name of the location where the concrete will be poured (e.g., foundation, column, slab).
+                            </li>
+                            <li>
+                                Concrete Strength: Select the desired concrete strength from the dropdown menu.
+                            </li>
+                            <li>
+                                Number of Items: Specify the number of concrete pouring locations for each row (up to a maximum of 100).
+                            </li>
+                            <li>
+                                Dimensions: Provide the length, width, and height (in meters) of the area where the concrete will be poured (each dimension can be up to 1000 meters).
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Calculate the Quantity and Price
+                        <ul>
+                            <li>
+                                Click the Calculate button to view the following:
+                                <ul>
+                                    <li>The concrete quantity and price for each row.</li>
+                                    <li>The total quantity and total price at the bottom of the form.</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>Proceed to Checkout:
+                        <ul>
+                            <li>Once all details are correct, click the Checkout button to proceed to the bill page and finalize your order.</li>
+                        </ul>
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+
+
         <button onClick={
             ()=>{
             setTimeout(() => {
             navigate('/company/home/concrete-orders/custom-orders');
-        }, 500)}}>
+        }, 500)}}
+        className='{}'
+        >
 
             next
         </button>
